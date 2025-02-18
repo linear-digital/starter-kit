885040c67abb7cb766697cf7c77b2b8fb8381b69
Linear Digital (Tamiz) <115143466+linear-digital@users.noreply.github.com>
2025-02-19 02:28:00 +0600

    Update _document.tsx

--- packages/blog-starter-kit/themes/hashnode/pages/_document.tsx
+++ packages/blog-starter-kit/themes/hashnode/pages/_document.tsx
@@ -3,7 +3,17 @@ import { Head, Html, Main, NextScript } from 'next/document';
 export default function Document() {
 	return (
 		<Html lang="en">
-			<Head />
+			<Head>
+				{/* Google AdSense Meta Tag */}
+				<meta name="google-adsense-account" content="ca-pub-4236044300590126" />
+
+				{/* Google AdSense Script */}
+				<script
+					async
+					src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4236044300590126"
+					crossOrigin="anonymous"
+				></script>
+			</Head>
 			<body>
 				<Main />
 				<NextScript />
