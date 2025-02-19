fc92f9789e6269a08619a4739d4447e1cdf93d02
Linear Digital (Tamiz) <115143466+linear-digital@users.noreply.github.com>
2025-02-19 11:43:50 +0000

    dsd

--- packages/blog-starter-kit/themes/hashnode/pages/_document.tsx
+++ packages/blog-starter-kit/themes/hashnode/pages/_document.tsx
@@ -13,6 +13,21 @@ export default function Document() {
 					src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4236044300590126"
 					crossOrigin="anonymous"
 				></script>
+				<script id="high-performance-format">
+					{`
+            atOptions = {
+              'key': '92dba182dfda009585811c011359acd6',
+              'format': 'iframe',
+              'height': 60,
+              'width': 468,
+              'params': {}
+            };
+          `}
+				</script>
+				<script
+					type="text/javascript"
+					src="//www.highperformanceformat.com/92dba182dfda009585811c011359acd6/invoke.js"
+				></script>
 			</Head>
 			<body>
 				<Main />
